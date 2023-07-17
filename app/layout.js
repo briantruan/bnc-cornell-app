ef00c4fdafc5c0b398d570c4325523f41c4da909
briantruan <86815054+briantruan@users.noreply.github.com>
2023-07-17 00:13:19 -0400

    Edit eslint config

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={montserrat.className}>
         <div>
-          <Sidebar children={children}/> // eslint-disable-line
+          <Sidebar children={children}/>
         </div>
       </body>
     </html>
