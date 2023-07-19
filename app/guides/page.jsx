f73cee250c681b51df0e3f11a1fe8ebcbf5d3172
briantruan <86815054+briantruan@users.noreply.github.com>
2023-07-19 13:57:16 -0400

    Attempt to fix revalidation issue

--- app/guides/page.jsx
+++ app/guides/page.jsx
@@ -6,6 +6,10 @@ export const metadata = {
   title: "Guides",
 };
 
+{/* This may be a work-around the cache issue, but we'll see */}
+
+export const revalidate = 60
+
 export default async function GuidesPage() {
   const response = await cachedClient(postsQuery);
 
